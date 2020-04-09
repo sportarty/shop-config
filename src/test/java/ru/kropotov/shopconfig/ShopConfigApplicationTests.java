a2c362ef6b7e4bfca053083a79ca5522f41024b0
kropotov <sportarty@mail.ru>
2020-04-10 01:05:03 +0300

    Базовый проект с конфигами

---
+++ src/test/java/ru/kropotov/shopconfig/ShopConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kropotov.shopconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
